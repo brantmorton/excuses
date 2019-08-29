ec35fafde04142cab70be0d68fb692f91281870d
brantmorton <brantmort@gmail.com>
2019-08-28 19:48:23 -0500

    deleted obsolete files

--- frontend/gui/src/App.test.js
+++ frontend/gui/src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme'
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+
+test('add', () => {
+  expect(1 + 2).toBe(3)
+})
+
+describe('ExcuseComponent', () => {
+  it('Ensures Generator button is clicked', () => {
+    const wrapper = shallow(<App />)
+    const generateButton = wrapper.find('button.btn')
+    generateButton.simulate('click')
+    const text = wrapper.find('h1').text()
+    expect(text).not.toBe('Generate')
+  })
+})
